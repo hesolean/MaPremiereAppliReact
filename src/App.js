c3e8207224023664d65c9eef1d8f302f915ecbd7
unknown <lndubourg@gmail.com>
2023-08-28 15:08:43 +0200

    ajout d'un prop

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  const subject = props.subject;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World
+          Hello {subject} !
         </p>
         
       </header>
