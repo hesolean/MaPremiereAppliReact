ee891e414ccd5fe4b7db9b615e8089a9a75f0f43
unknown <lndubourg@gmail.com>
2023-08-28 16:44:15 +0200

    modification de App pour intégrer les nouveaux composants à la place des balises

--- src/App.js
+++ src/App.js
@@ -18,39 +18,11 @@ function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            Qu'y a-t-il à faire&nbsp;?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Ajouter
-        </button>
-      </form>
+      <Form />
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Montrer </span>
-          <span>Toutes</span>
-          <span className="visually-hidden"> les tâches</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Montrer </span>
-          <span className="visually-hidden">les tâches </span>
-          <span>Actives</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Montrer </span>
-          <span className="visually-hidden">les tâches </span>
-          <span>Terminées</span>
-        </button>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
       <h2 id="list-heading">3 tâches restantes</h2>
       <ul
